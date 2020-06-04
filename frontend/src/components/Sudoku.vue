ae02e4bf22bc22a1bba0709a2e122ba56d033688
Alejandroacho <alejandroacho@hotmail.com>
2020-06-05 00:28:13 +0200

    Front mejorado, inputs por poco

--- frontend/src/components/Sudoku.vue
+++ frontend/src/components/Sudoku.vue
@@ -2,42 +2,47 @@
     <div class="content">
         <h1>{{ title }}</h1>
         
-        <div class="grid" >
-            <div class="row" v-for="(row, rowIndex) in sudoku" :key="rowIndex"> 
-                <div class="cell" :class="{'border-right': colIndex === 2 || colIndex === 5, 
+        <div>
+            <form>
+            <table class="game">
+               
+                <tr class="row" v-bind:key="row" v-for="(row, rowIndex) in sudoku">
+                    <td class="cell" :class="{'border-right': colIndex === 2 || colIndex === 5, 
                                            'border-bottom': rowIndex === 2 || rowIndex === 5, 
-                                           'original': cell.original,
-                                           'active': activeRow === rowIndex && activeCol === colIndex, }"
-                v-for="(cell, colIndex) in row" :key="colIndex"
-                @click="setCellActive(rowIndex, colIndex, cell.original)">
-                    <input type="number" min="1" max="9" maxlength="3" v-if="cell.value === null">
-                    {{ cell.value }}
-                </div>
-            </div>
+                                           }"
+                    v-bind:key="number" v-for="(number, colIndex) in row" >
+
+                        <input name="" :value="number" :id="'row' + rowIndex + 'column' + colIndex" type="number" v-if="number != 0" readonly class="original">
+
+                        <input name="" :id="'row' + rowIndex + 'column' + colIndex" type="number" min="1" max="9" maxlength="1" v-if="number === 0">
+                    
+                    </td>
+                </tr>
+                <br>  
+            </table>
+            <button type="submit" v-if="timerInit" class="verify">
+                Resvisar
+            </button>
+            </form>
         </div>
-        
-        <button @click="getSudoku">
+        <br>
+        <button @click="startNewGame" class="newmatch">
             Nueva partida
         </button>
 
-        
-        <!-- TIMER -->
-        <div class="timer">
-            <span class="elapsed" :class="{blinking : timer == null}">
+    
+         <div class="timer">
+            <span class="elapsed">
                 <span v-if="hours>0">{{ hours }}:</span>{{ minutes }}:{{ seconds }}
             </span>
-
-            <!-- Start Timer -->
-            <button class="timer-btns" id="start" v-if="!timer" @click="startTimer">
-                <img src="../assets/images/play-btn.png"> 
-            </button>
-            
-            <!-- Pause Timer -->
             <button class="timer-btns" id="stop" v-if="timer" @click="stopTimer">
                 <img src="../assets/images/pause.png">
             </button>
-
-        </div>   
+            <button class="timer-btns" id="start" v-if="showTimerVar && !timer" @click="showTimer">
+                <img src="../assets/images/play-btn.png"> 
+            </button>
+            <br>
+        </div>  
     </div>
         
 
@@ -46,33 +51,42 @@
 </template>
 
 <script>
-
     export default {
         name: "Sudoku",
         data() {
             return {
-                title: "sudoku",
+                title: "Sudoku Go",
                 message: " ",
                 sudoku: [],
+                inputs: [],
                 activeRow: -1,
                 activeCol: -1,
                 timer: null,
+                timerInit:null,
                 totalTime: 0,
+                showTimerVar: null,
+                name: ""
             };
         },
         methods: {
-            getSudoku: function() {
-                var self = this;
-                window.backend.generateSudoku().then(result => {
-                    self.sudoku = result.map(row => {
-                        return row.map(cell => {
-                            return {
-                                value: cell !== 0 ? cell : null,
-                                original: cell !== 0
-                            }
-                        })
-                    });
-                });
+            getBoard: function() {
+                 window.backend.generateSudoku().then(result => {
+                 this.sudoku = result;
+                 });
+            },
+            startNewGame: function() { 
+                this.getBoard();
+                this.stopTimer();
+                this.resetTimer();
+                this.showTimer();
+            },
+            showTimer(){
+                this.timerInit=true
+                this.showTimerVar=true
+                this.startTimer();
+            },
+            resetTimer: function(){
+                this.totalTime=0
             },
             setCellActive(row, col, original) {
                 if (original) {
@@ -118,51 +132,50 @@
                 return this.padTime(seconds);   
             }
         }   
-    }    
+    }   
+
 </script>
 
 <style scoped>
     
     /* sudoku */
-    .grid {
+    .game {
+        margin-left: 32%;
         width: calc(9*40px);
         margin: 0.5rem, auto;
     }
-
     .row {
         display: flex;
         align-items: center;
         justify-content: space-between;
     }
-
     .cell {
         display: block;
         width: 40px;
         height: 40px;
         box-sizing: border-box;
-        border: 1px solid #888;
+        border: 1px solid rgb(58, 58, 58);
+        background-color: #ccc;
 
         line-height: 40px;
         text-align: center;
     }
-
     .cell.border-right {
         border-right-width: 5px;    
     }
     .cell.border-bottom {
         border-bottom-width: 5px;    
     }
-    .cell.original {
+    .original {
+        color: black;
         font-weight: bold;
     }
     .cell:not(.original) {
         cursor: pointer;
     }
-    .cell.active {
-        background-color: #888;
-        color: black
+    form{
+        align-content: center;
     }
-
     input {
         width: 32px;
         height: 35px;
@@ -171,31 +184,71 @@
         padding: 0;
         border: 3px transparent solid;
         background-color: transparent;
-        color: #eee;
+        color: rgb(75, 75, 75);
     }
-
     /* timer */
     .timer {
         font-size: 2em;
-        color: #ccc;
+        color: #41BF98;
         text-align: center;
         padding:15px;
         margin:0 auto;
     }
-
     .timer-btns {
         border:none;
-        color: #ccc;
+        color: #41BF98;
         font-size:1em;
         background-color: transparent;
         cursor: pointer;
-    }:hover {
-        color: #888;
-    } 
+    }
     .timer-btns img {
         width: 1em;
         height: 1em;
-    
     }
-
+    /*Botones */
+    .newmatch {
+	background:linear-gradient(to bottom, #404040 5%, #6b6b6b 100%);
+	background-color:#404040;
+	border-radius:18px;
+	border:2px solid #41bf97;
+	display:inline-block;
+	cursor:pointer;
+	color:#3fdfac;
+	font-family: "Roboto", Helvetica, Arial, sans-serif;
+	font-size:15px;
+	padding:10px 40px;
+	text-decoration:none;
+    }
+    .newmatch:hover {
+	background:linear-gradient(to bottom, #6b6b6b 5%, #404040 100%);
+	background-color:#6b6b6b;
+    }
+    .newmatch:active {
+	position:relative;
+    top:1px;
+    }
+    .verify {
+    align-self: center;
+	box-shadow: 0px 10px 14px -7px #276873;
+	background:linear-gradient(to bottom, #41bf97 5%, #318f70 100%);
+	background-color:#41bf97;
+	border-radius:8px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:"Roboto", Helvetica, Arial, sans-serif;
+    font-size:15px;
+    height: 60px;
+    width: 170px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #3d768a;
+    }
+    .verify:hover {
+	background:linear-gradient(to bottom, #318f70 5%, #41bf97 100%);
+	background-color:#318f70;
+    }
+    .verify:active {
+	position:relative;
+	top:1px;
+    }
 </style>
--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-  "github.com/leaanthony/mewn"
-  "github.com/wailsapp/wails"
-  "sudoku/pkg/sudoku"
+	"sudoku/pkg/sudoku"
+
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
 )
-
-func basic() string {
-  return "Cacahuete"
+//Cell object
+type Cell struct {
+	value     int
+	column    int
+  row       int
+  isVisible bool // true: se ve el número y false: no se muestra el núm
 }
-
 func generateSudoku() [9][9]int {
-  board:=sudoku.SudokuGenerator()
+	board := sudoku.SudokuGenerator()
 	return board
 }
 
 func main() {
-  js := mewn.String("./frontend/dist/app.js")
-  css := mewn.String("./frontend/dist/app.css")
+	js := mewn.String("./frontend/dist/app.js")
+	css := mewn.String("./frontend/dist/app.css")
 
-  app := wails.CreateApp(&wails.AppConfig{
-    Width:  1024,
-    Height: 768,
-    Title:  "Sudoku",
-    JS:     js,
-    CSS:    css,
-    Colour: "#131313",
-  })
-  app.Bind(generateSudoku)
-  app.Run()
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:  1024,
+		Height: 800,
+		Title:  "Sudoku",
+		JS:     js,
+		CSS:    css,
+		Colour: "#131313",
+	})
+	app.Bind(generateSudoku)
+	app.Run()
 }
